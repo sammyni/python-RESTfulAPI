75e149bb1877391f0487ee4c1cbbacd897f9f61b
Ndubuisi Samuel <samniwebdev@gmail.com>
2019-01-19 09:39:08 +0100

    Added environment configs

--- app.py
+++ app.py
@@ -1,37 +1,30 @@
 # Created by Samuel Ndubuisi
 # On 12th January, 2019
+# Credits: https://realpython.com/flask-connexion-rest-api/, https://realpython.com/flask-connexion-rest-api-part-2/
 
 # Python3
 # Environment = ussd-app
 
-from config import Config as AppConfig;
 
-# from flask import (
-#     Flask,
-#     render_template as Render
-# )
 
+# Import modules
+import os
 from flask import render_template as Render
-
 import connexion
+from flask_sslify import SSLify # import SSLify for SSL Handling
 
-# Import environment class
-from config import ProductionConfig as ModeConfig
 
-# import SSLify for SSL Handling
-from flask_sslify import SSLify
+AppConfig = getattr(__import__('config', fromlist=[os.environ['APP_SETTINGS']]), os.environ['APP_SETTINGS'])
 
 # Application instance
-# app = Flask(__name__, template_folder="templates")
-# app.config.from_object(config.DevelopmentConfig)
 app  = connexion.App(__name__, specification_dir='./')
 
+
 # Implement SSL Handling
-# SSLify(app)
+SSLify(app.app)
+
 # Read the Swagger.yml file to configure endpoints
 app.add_api('swagger.yml')
-
-
 
 
 # Create URL route for "/"
@@ -42,4 +35,4 @@ def home():
     return Render('home.html')
 
 if __name__ == '__main__':
-    app.run(host=AppConfig.HOST)
+    app.run(host=AppConfig.HOST, port=AppConfig.PORT, debug=AppConfig.DEBUG)
--- config.py
+++ config.py
@@ -3,17 +3,31 @@
 
 # Application configuration
 
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     """docstring for Config."""
-    PORT = 5000
+    PORT = 8080
     HOST = '0.0.0.0'
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
 
 
-class ProductionConfig(object):
+class ProductionConfig(Config):
     """docstring for ProductionConfig."""
     DEBUG = False
 
 
-class DevelopmentConfig(object):
+class DevelopmentConfig(Config):
     """docstring for DevelopmentConfig."""
+    DEVELOPMENT = True
     DEBUG = True
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
